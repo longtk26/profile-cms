dd0796e408b6bf3092e04a2e2072fd0e4b0ce583
ngoclong <ngoclong2663@gmail.com>
2025-01-12 00:15:01 +0700

    feat: config post schema

---
+++ schemaTypes/postType.ts
@@ -0,0 +1,56 @@
+import {defineArrayMember, defineField, defineType} from 'sanity'
+
+export const postType = defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'object',
+          name: 'tag',
+          fields: [{type: 'string', name: 'value'}],
+        }),
+      ],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'updatedAt',
+      title: 'Updated at',
+      type: 'datetime',
+    }),
+    defineField({
+        name: 'content',
+        title: 'Content',
+        type: 'array',
+        of: [
+            {
+                type: 'block',
+                styles: [
+                    {title: 'Normal', value: 'normal'},
+                    {title: 'H1', value: 'h1'},
+                    {title: 'H2', value: 'h2'},
+                    {title: 'H3', value: 'h3'},
+                    {title: 'H4', value: 'h4'},
+                    {title: 'Quote', value: 'blockquote'},
+                ],
+            },
+            {type: 'image'},
+            {type: 'code'},
+        ],
+    })
+  ],
+})
